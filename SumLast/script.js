74b03d3d74242966e9ce59c1141068d0d044e130
imac <leman.mammadly23@gmail.com>
2023-06-08 09:35:14 +0400

    last

--- SumLast/script.js
+++ SumLast/script.js
@@ -2,16 +2,14 @@
 
 let arr = [1, 2, 3];
 function SumLastThree() {
-  let sum;
+let sum;
 
   for (let i = 9; i > 0; i--) {
     let nums = arr.slice(-3);
-    for (let j = 0; j < arr.length; j++) {
-      sum = nums.reduce((a, b) => a + b, 0);
-    }
+    sum = nums.reduce((a, b) => a + b, 0);
     arr.push(sum);
   }
 }
 
-SumLastThree();
 console.log(arr);
+SumLastThree();
